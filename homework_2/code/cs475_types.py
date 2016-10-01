581390ad674eafe1bfca37fa7c256b05ef1be4d9
Eric Bridgeford <ebridge2@jhu.edu>
2016-10-01 19:13:00 -0400

    added hw2 code; margin working

---
+++ homework_2/code/cs475_types.py
@@ -0,0 +1,63 @@
+from abc import ABCMeta, abstractmethod
+
+# abstract base class for defining labels
+class Label:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __str__(self): pass
+
+       
+class ClassificationLabel(Label):
+    def __init__(self, label):
+        self.label = label
+        pass
+        
+    def __str__(self):
+        return str(self.label)
+
+class FeatureVector:
+    def __init__(self):
+      	self.feature_dict = {}
+        
+    def add(self, index, value):
+        self.feature_dict[index] = value
+        pass
+        
+    def get(self, index):
+        return self.feature_dict[index]
+
+    def __iter__(self):
+        return iter(self.feature_dict)
+
+    def max_feature(self):
+        return max(self.feature_dict.keys(), key=int)
+
+class Instance:
+    def __init__(self, feature_vector, label):
+        self._feature_vector = feature_vector
+        self._label = label
+
+    def __iter__(self):
+        return iter(self._feature_vector)
+
+    def get_label(self):
+        return str(self._label)
+
+    def get(self, idx):
+        return self._feature_vector.get(idx)
+
+    def max_feature(self):
+        return self._feature_vector.max_feature()
+
+# abstract base class for defining predictors
+class Predictor:
+    __metaclass__ = ABCMeta
+
+    def __init__(self): pass
+
+    @abstractmethod
+    def train(self, instances): pass
+
+    @abstractmethod
+    def predict(self, instance): pass 
---
+++ homework_2/code/perceptron.py
@@ -0,0 +1,124 @@
+# perceptron.py
+#
+# written by Eric Bridgeford
+#
+# a class for abstracting a perceptron to
+# either a standard perceptron or a
+# averaged perceptron model.
+
+from cs475_types import Predictor
+from abc import ABCMeta, abstractmethod
+import numpy as np
+
+
+class Perceptron_Base(Predictor):
+    __metaclass__ = ABCMeta
+
+    def __init__(self, rate, iterations):
+        super(Perceptron_Base, self).__init__()
+        self.nfeatures = 1
+        self.rate = rate
+        self.iterations = iterations
+        self.w = np.zeros(self.nfeatures)
+        # define label to signed
+        self.l2s_dict = {'0': -1, '1': 1}
+        # define the signed to label
+        self.s2l_dict = {-1: '0', 1: '1', 0: '1'}
+        pass
+
+    @abstractmethod
+    def train(self, instances): pass
+
+    def predict(self, instance):
+        example = self.load_example(instance)
+        self.check_dims(example)
+        return self.s2l_dict[self.predict_sgn(example, self.w)]
+
+    def dot_weight(self, example, weight):
+        # load the example and compute the sign of the sum of
+        # the dot product
+        return np.sign(np.dot(weight, example))
+
+    def predict_sgn(self, example, weight):
+        value = self.dot_weight(example, weight)
+        if (value >= 0):
+            return 1
+        else:
+            return -1
+
+    def load_example(self, instance):
+        example = np.zeros(max(instance.max_feature(), self.nfeatures))
+        for idx in instance:
+            # since we have 1 indexed features and 0-indexed arrays
+            example[idx - 1] = instance.get(idx)
+        return example
+
+    def check_dims(self, example, weight=None):
+        inst_feat = example.shape[0]
+        if (inst_feat > self.nfeatures):
+            self.w = np.pad(self.w, (0, inst_feat - self.nfeatures),
+                'constant', constant_values=0)
+            self.nfeatures = inst_feat
+            if (weight is not None):
+                weight = np.pad(weight, (0, inst_feat - weight.shape[0]),
+                            'constant', constant_values=0)
+        if (weight is not None):
+            return weight
+        else:
+            return self.w
+
+
+class Perceptron(Perceptron_Base):
+
+    def __init__(self, rate, iterations):
+        super(Perceptron, self).__init__(rate, iterations)
+        pass
+
+    def train(self, instances):
+        for iteration in range(0, self.iterations):
+            for instance in instances:
+                example = self.load_example(instance)
+                self.check_dims(example)
+                yhat = self.predict_sgn(example, self.w)
+                y = self.l2s_dict[instance.get_label()]
+                if (yhat != y):
+                    self.w = self.w + self.rate * y * example
+        pass
+
+
+class Weighted_Perceptron(Perceptron_Base):
+
+    def __init__(self, rate, iterations):
+        super(Weighted_Perceptron, self).__init__(rate, iterations)
+        pass
+
+    def train(self, instances):
+        w_round = self.w.copy()
+        for iteration in range(0, self.iterations):
+            for instance in instances:
+                example = self.load_example(instance)
+                w_round = self.check_dims(example, weight=w_round)
+                yhat = self.predict_sgn(example, w_round)
+                y = self.l2s_dict[instance.get_label()]
+                if (yhat != y):
+                    w_round = w_round + self.rate * y * example
+                self.w += w_round
+        pass
+
+
+class Margin_Perceptron(Perceptron_Base):
+
+    def __init__(self, rate, iterations):
+        super(Margin_Perceptron, self).__init__(rate, iterations)
+        pass
+
+    def train(self, instances):
+        for iteration in range(0, self.iterations):
+            for instance in instances:
+                example = self.load_example(instance)
+                self.check_dims(example)
+                value = self.dot_weight(example, self.w)
+                y = self.l2s_dict[instance.get_label()]
+                if ((y*value) < 1):
+                    self.w = self.w + self.rate * y * example
+        pass
