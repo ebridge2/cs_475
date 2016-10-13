bbb1127ba622420dc94ba35fc896482779773c99
Eric Bridgeford <ebridge2@jhu.edu>
2016-10-13 06:01:44 -0400

    updated working but slow as shit

--- homework_3/code/adaboost.py
+++ homework_3/code/adaboost.py
@@ -23,8 +23,7 @@ class Adaboost(Predictor):
         self.nfeatures = 0
         pass
 
-    def learn_features(self, instances):
-        self.instances = instances
+    def learn_features(self):
         self.ninstances = len(self.instances)
         for instance in self.instances:
             self.nfeatures = np.max([self.nfeatures, instance.max_feature()])
@@ -42,141 +41,96 @@ class Adaboost(Predictor):
                           self.get_feat(instance, feature))) for instance in self.instances],
                           dtype=self.sort_dtype)
         return np.sort(values, order=['value', 'label'])
- 
-
 
     def make_hypothesis_class(self, instances):
-        self.learn_features(instances)
-        self.hypothesis_set = []
+        self.instances = instances
+        self.learn_features()
+        self.hyp_dtype =  dtype=[('feature', int), ('cutoff', float),
+                                 ('greater', int), ('less', int), ('error', float)]
+ 
+        # overallocate so we don't have to copy ever
+        self.hyp_set = np.array([tuple((0, 0, 0, 0, 0))
+                                for i in range(0, self.nfeatures*len(self.instances))],
+                                dtype = self.hyp_dtype)
+        count = 0 # default to zero and replace as needed to hypo set
         for feature_id in range(0, self.nfeatures):
             sorted_vals = self.get_feature_set(feature_id)
             # store the labels with the values so we can simplify hypothesis class significantly
             this_feature = np.zeros(0)
             for idx in range(0, sorted_vals.shape[0] - 1):
-                # if we have a new value, and if the label has changed
-                # note that we greatly simplify our hypothesis class. For example, if we have the following
-                # dataset: + + + + + + - - - - + - - - -
-                # we would never place a potential hypothesis btwn two + + or two - -, since there
-                # would in the weak classifier case always be a hypohthesis on the end that is 
-                # better
                 this_val = sorted_vals[idx]
-                # some clever logic tricks. There are a lot of possible situations to consider, and without
-                # building a datastructure that comprises unevenly linked lists btwn attributes (we need to
-                # consider situations where we have same label on the same value and skip making a hypothesis here,
-                # whether we have a same label as the next value to appear and then skip making a hypothesis here,
-                # and finally consider when we are the last of a string of same labels on a particular value
-                # the next label consists of a bunch of the same label, and then somewhere in there is a 
-                # different label on the next value to appear.
-                condition_ra_same_value = np.where(np.logical_and(sorted_vals['label'] == this_val['label'],
-                                                                  sorted_vals['value'] == this_val['value']))[0]
-                if (condition_ra_same_value.shape[0] != 0):
-                    # the next different labels to appear with value greater than our current value
-                    condition_idxs = np.where(np.logical_and(sorted_vals['label'] != this_val['label'],
-                                                             sorted_vals['value'] >= this_val['value']))[0]
-                    # if we have none of the other labels after this one, we are finished
-                    if (condition_idxs.shape[0] != 0):
-                        # the next same labels to appear with value greater than our current value
-                        condition_ra_next_same_label = np.where(np.logical_and(sorted_vals['label'] == this_val['label'],
-                                                                               sorted_vals['value'] > this_val['value']))[0]
-                        next_val_id = np.min(condition_idxs)
-                        if (condition_ra_next_same_label.shape[0] != 0):
-                            next_val_same_label = np.min(condition_ra_next_same_label)
-                            if (sorted_vals[next_val_id]['value'] > sorted_vals[next_val_same_label]['value']):
-                                next_val_id = None
-                    else:
-                        next_val_id = None
-                    if (next_val_id is not None):
-                        next_val = sorted_vals[next_val_id]
-                        # constrain hypothesis set greatly
-                        # first need to check whether we are at the end of our value chains
-                        # this prevents us from having a bunch of labels for one value and not
-                        # considering it any change from the next point
-                        # if there is a reason to add the feature, pick it
-                        # uses max-margin (never going to split when the labels are the same on both
-                        # sides of the cutoff)
-                        this_feature = np.union1d(this_feature, [float(np.mean([this_val['value'], next_val['value']]))])
+                next_val = sorted_vals[idx + 1]
+                cutoff = np.mean([this_val['value'], next_val['value']])
                 # put our potential hypotheses in a 0-indexed list
-            self.hypothesis_set.append(this_feature)
+                (gvals, gv_ct) = np.unique(sorted_vals[sorted_vals['value'] > cutoff]['label'],
+                                           return_counts=True)
+                (lvals, lv_ct) = np.unique(sorted_vals[sorted_vals['value'] <= cutoff]['label'],
+                                           return_counts=True)
+                if (lvals.shape[0] != 0) and (gvals.shape[0] != 0):
+                    greater = gvals[np.argmax(gv_ct)]
+                    less = lvals[np.argmax(lv_ct)]
+                    self.hyp_set[count] = tuple((feature_id, cutoff, greater, less, 0))
+                    count = count + 1
+        # return only as needed
+        self.hyp_set = self.hyp_set[0:count]
+        pass
 
-    def calculate_hypothesis(self, feat, c, sorted_vals):
-        possible_values = np.zeros(self.unique_labels.shape[0])
-        if (feat >= c):
-            for label_id in range(0, self.unique_labels.shape[0]):
-                possible_values[label_id] = np.where(np.logical_and(sorted_vals['value'] >= c,
-                                                    sorted_vals['label'] == self.unique_labels[label_id]
-                                                    ))[0].shape[0]
+    def calculate_hyp(self, hypothesis, value):
+        if (value > hypothesis['cutoff']):
+            return hypothesis['greater']
         else:
-            for label_id in range(0, self.unique_labels.shape[0]):
-                possible_values[label_id] = np.where(np.logical_and(sorted_vals['value'] < c,
-                                                    sorted_vals['label'] == self.unique_labels[label_id]
-                                                    ))[0].shape[0]
-        return self.unique_labels[np.argmax(possible_values)]
+            return hypothesis['less']
  
     def train(self, instances):
         self.make_hypothesis_class(instances)
-        dtype_hyp = [('feature', int), ('cutoff', float), ('error', float)]
-        self.hyp = np.array([tuple((0, 0, 0)) for i in range(0, self.iter_boost)],
-                            dtype=dtype_hyp)
+        self.hyp = np.array([tuple((0, 0, 0, 0, 0))
+                                    for i in range(0, self.iter_boost)],
+                            dtype=self.hyp_dtype)
+        self.alpha = np.array([1 for i in range(0, self.iter_boost)], dtype=float) # initialize our weight vecs
         D = np.zeros(self.ninstances)
         D.fill(1/float(self.ninstances))
         t = 0
         tol = 1 # default to worst case unless data tells us otherwise
-        self.alpha = np.ones(self.iter_boost) # initialize our weight vecs
         while (t < self.iter_boost and tol > .000001):
             # one best hypothesis per feature will be compared
-            hyp_c = np.array([tuple((0, 0, 0)) for i in range(0, self.nfeatures)],
-                             dtype=dtype_hyp)
-            for j in range(0, len(self.hypothesis_set)):
-                feat_err = np.array([tuple((j, i, 0)) for i in self.hypothesis_set[j]],
-                                       dtype = dtype_hyp)
-                sorted_vals = self.get_feature_set(j)
-                for i in range(0, len(self.instances)):
-                    example = self.instances[i]
-                    feat = self.get_feat(example, j)
-                    y = self.get_label(example)
-                    for c in range(0, len(self.hypothesis_set[j])):
-                        cutoff = feat_err[c]['cutoff']
-                        yhat = self.calculate_hypothesis(feat, cutoff, sorted_vals)
-                        feat_err[c]['error'] = feat_err[c]['error'] + D[i]*float(yhat != y)
-                hyp_c[j] = np.sort(feat_err, order=('error', 'cutoff'))[0] # lowest error
-            sorted_feat_hyps = np.sort(hyp_c, order=('error', 'feature'))
+            for idx in range(0, self.hyp_set.shape[0]):
+                new_err = 0
+                feature = self.hyp_set[idx]['feature']
+                examples = self.get_feature_set(feature)
+                for i in range(0, examples.shape[0]):
+                    example = examples[i]
+                    yhat = self.calculate_hyp(self.hyp_set[idx], example['value'])
+                    new_err += D[i]*float(yhat != example['label'])
+                self.hyp_set[idx]['error'] = new_err
+            sorted_feat_hyps = np.sort(self.hyp_set, order=('error', 'feature'))
             self.hyp[t] = sorted_feat_hyps[0] # lowest error
             tol = self.hyp[t]['error'] # tolerance is our error
             if (tol != 0): # in case we have a divide by zero
                 best_feature = self.hyp[t]['feature']
                 feature_sorted_vals = self.get_feature_set(best_feature)
-                self.alpha[t] = .5 * np.log((1-tol)/tol)
+                self.alpha[t] = .5*np.log((1-tol)/tol)
                 power_ar = np.array([-self.alpha[t] *\
-                                     self.calculate_hypothesis(self.get_feat(example,best_feature),
-                                                               self.hyp[t]['cutoff'],
-                                                               feature_sorted_vals) * self.get_label(example)
-                                     for example in self.instances])
+                                     self.calculate_hyp(self.hyp[t], example['value']) * example['label']
+                                     for example in feature_sorted_vals])
                 exp_term = np.exp(power_ar)
                 Z = np.dot(D, exp_term).sum()
                 D = 1/float(Z) * np.multiply(D, exp_term)
             t = t + 1
         self.hyp = self.hyp[0:t]
+        print self.hyp
         self.alpha = self.alpha[0:t]
+        print self.alpha
         pass
 
     def predict(self, instance):
-#        print "NEXT!"
         dtype_predict = [('value', float), ('label', int)]
         possible_labels = np.array([tuple((0, label)) for label in self.unique_labels],
                                    dtype=dtype_predict)
-#        print self.hyp
-#        print self.get_feature_set(self.hyp[0]['feature'])
-#        print self.get_feat(instance, self.hyp[0]['feature'])
-        hypo_per_t = np.array([self.calculate_hypothesis(self.get_feat(instance,
-                                                                       self.hyp[t]['feature']),
-                                                         self.hyp[t]['cutoff'],
-                                                         self.get_feature_set(self.hyp[t]['feature']))
+        hypo_per_t = np.array([self.calculate_hyp(self.hyp[t], self.get_feat(instance, self.hyp[t]['feature']))
                               for t in range(0, self.hyp.shape[0])])
-#        print hypo_per_t
         for i in range(0, possible_labels.shape[0]):
-            votes = np.array((hypo_per_t == possible_labels[i]['label'])).astype(int) # number of matches
-#            print votes
+            votes = np.array(hypo_per_t == possible_labels[i]['label']).astype(int) # number of matches
+            print "votes" + str(votes)
             possible_labels[i]['value'] = np.dot(self.alpha, votes)
-#        print possible_labels
         yhat = np.sort(possible_labels, order=('value', 'label'))[possible_labels.shape[0]- 1]
         return self.l2s_key[yhat['label']]
