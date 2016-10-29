0d91ce1bbcddae4ac649f97d1893378a991b92b4
Eric Bridgeford <ebridge2@jhu.edu>
2016-10-29 19:59:37 -0400

    updated means working, not correct though

--- homework_4/means.py
+++ homework_4/means.py
@@ -18,16 +18,17 @@ class Lambda_Means(Predictor):
         self.iterations = iterations
         self.K = 1
         self.nfeatures = 0
+        pass
 
     # load the example into an array
-    def load_example(self.instance):
+    def load_example(self, instance):
         example = np.zeros((self.nfeatures))
         for idx in instance:
             # since we have 1 indexed features but zero indexed arrays
             example[idx-1] = instance.get(idx)
         return example[:self.nfeatures]
 
-    def get_feature(self, instance):
+    def get_feature(self, instance, feature):
         return instance.get(feature + 1, default=0)
 
     def calculate_max_feature(self):
@@ -40,16 +41,19 @@ class Lambda_Means(Predictor):
     # vector.
     def get_feature_mean(self, resp, feat_id):
         this_feature = np.ones((self.ninstances))
-        for instance in range(0, self.ninstances):
-            this_feature[idx] = self.get_feature(instance[idx], feat_id)
+        instances = self.instances
+        for idx in range(0, self.ninstances):
+            this_feature[idx] = self.get_feature(instances[idx], feat_id)
         return np.mean(this_feature)
 
     def initialize(self):
         # will be the mean of all of these
-        self.resp = np.ones((self.ninstances, self.K))         
-        self.means = np.zeros((self.nfeatures, 1))
-        for feature in range(0, nfeatures):
-            self.means[feature, 1] = self.get_feature_mean(self, resp, feature)
+        resp = np.ones((self.ninstances, self.K))         
+        means = np.zeros((self.nfeatures, 1))
+        for feature in range(0, self.nfeatures):
+            means[feature, 0] = self.get_feature_mean(resp, feature)
+        self.resp = resp
+        self.means = means
         pass
 
     # a function to compute the distance between a single datum and
@@ -58,7 +62,7 @@ class Lambda_Means(Predictor):
         K = self.K
         distances = np.zeros((K))
         for k in range(0, K):
-            distances[k] = distance.euclidian(x, means[:,k])
+            distances[k] = distance.euclidean(x, means[:,k])**2
         return distances
 
     def get_responsibilities(self, lambd):
@@ -66,6 +70,7 @@ class Lambda_Means(Predictor):
         # load the means locally for speed
         means = self.means
         for idx in range(0, self.ninstances):
+            print "resp" + str(idx)
             x = self.load_example(self.instances[idx])
             distance = self.compute_distance(x, means)
             distance = distance[distance <= lambd]
@@ -78,12 +83,12 @@ class Lambda_Means(Predictor):
             # make a new cluster if none of the current clusters
             # satisfy the lambda requirement
             else:
-                means = np.hstack(means, x)
+                means = np.column_stack((means, x))
                 # add a responsibility vector that defaults to zeros
                 # and set the value of this instance to 1
                 new_rvec = np.zeros((self.ninstances, 1))
                 new_rvec[idx] = 1
-                r = np.hstack(r, new_rvec)
+                r = np.column_stack((r, new_rvec))
                 self.K = self.K + 1
         self.means = means
         self.resp = r
@@ -96,13 +101,17 @@ class Lambda_Means(Predictor):
         means = np.zeros((self.nfeatures, K))
         instances = self.instances
         for k in range(0, K):
+            print "means" + str(k)
             r_vec = r[:,k]
             # get the instance ids we want so we don't have to
             # check all of them
             r_ids = np.where(r_vec != 0)[0]
             for idx in r_ids:
                 means[:,k] += self.load_example(instances[idx])
-            means[:,k] = means[:,k]/r_ids.shape[0]
+            if (r_ids.shape[0] != 0):
+                means[:,k] = means[:,k]/r_ids.shape[0]
+            else:
+                means[:,k] = means[:,k]
         # update with our maximized means
         self.means = means
         pass
@@ -113,6 +122,7 @@ class Lambda_Means(Predictor):
         # initialize the parameters of the model (resps and means)
         self.initialize()
         for i in range(0, self.iterations):
+            print i
             # get the respnsibilities, E step
             self.get_responsibilities(self.lambd)
             # maximize the means, M step
