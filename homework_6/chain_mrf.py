a1efcf058e7d33bd397aafa7758e19fc36527211
Eric Bridgeford <ebridge2@jhu.edu>
2016-12-06 00:02:54 -0500

    updated to add hw6 folder

---
+++ homework_6/chain_mrf.py
@@ -0,0 +1,246 @@
+import numpy as np
+
+
+class ChainMRFPotentials:
+    def __init__(self, data_file):
+        with open(data_file) as reader:
+            for line in reader:
+                if len(line.strip()) == 0:
+                    continue
+
+                split_line = line.split(" ")
+                try:
+                    self._n = int(split_line[0])
+                except ValueError:
+                    raise ValueError("Unable to convert " + split_line[0] + " to integer.")
+                try:
+                    self._k = int(split_line[1])
+                except ValueError:
+                    raise ValueError("Unable to convert " + split_line[1] + " to integer.")
+                break
+
+            # create an "(n+1) by (k+1)" list for unary potentials
+            self._potentials1 = [[-1.0] * ( self._k + 1) for n in range(self._n + 1)]
+            # create a "2n by (k+1) by (k+1)" list for binary potentials
+            self._potentials2 = [[[-1.0] * (self._k + 1) for k in range(self._k + 1)] for n in range(2 * self._n)]
+
+            for line in reader:
+                if len(line.strip()) == 0:
+                    continue
+
+                split_line = line.split(" ")
+
+                if len(split_line) == 3:
+                    try:
+                        i = int(split_line[0])
+                    except ValueError:
+                        raise ValueError("Unable to convert " + split_line[0] + " to integer.")
+                    try:
+                        a = int(split_line[1])
+                    except ValueError:
+                        raise ValueError("Unable to convert " + split_line[1] + " to integer.")
+                    if i < 1 or i > self._n:
+                        raise Exception("given n=" + str(self._n) + ", illegal value for i: " + str(i))
+                    if a < 1 or a > self._k:
+                        raise Exception("given k=" + str(self._k) + ", illegal value for a: " + str(a))
+                    if self._potentials1[i][a] >= 0.0:
+                        raise Exception("ill-formed energy file: duplicate keys: " + line)
+                    self._potentials1[i][a] = float(split_line[2])
+                elif len(split_line) == 4:
+                    try:
+                        i = int(split_line[0])
+                    except ValueError:
+                        raise ValueError("Unable to convert " + split_line[0] + " to integer.")
+                    try:
+                        a = int(split_line[1])
+                    except ValueError:
+                        raise ValueError("Unable to convert " + split_line[1] + " to integer.")
+                    try:
+                        b = int(split_line[2])
+                    except ValueError:
+                        raise ValueError("Unable to convert " + split_line[2] + " to integer.")
+                    if i < self._n + 1 or i > 2 * self._n - 1:
+                        raise Exception("given n=" + str(self._n) + ", illegal value for i: " + str(i))
+                    if a < 1 or a > self._k or b < 1 or b > self._k:
+                        raise Exception("given k=" + self._k + ", illegal value for a=" + str(a) + " or b=" + str(b))
+                    if self._potentials2[i][a][b] >= 0.0:
+                        raise Exception("ill-formed energy file: duplicate keys: " + line)
+                    self._potentials2[i][a][b] = float(split_line[3])
+                else:
+                    continue
+
+            # check that all of the needed potentials were provided
+            for i in range(1, self._n + 1):
+                for a in range(1, self._k + 1):
+                    if self._potentials1[i][a] < 0.0:
+                        raise Exception("no potential provided for i=" + str(i) + ", a=" + str(a))
+            for i in range(self._n + 1, 2 * self._n):
+                for a in range(1, self._k + 1):
+                    for b in range(1, self._k + 1):
+                        if self._potentials2[i][a][b] < 0.0:
+                            raise Exception("no potential provided for i=" + str(i) + ", a=" + str(a) + ", b=" + str(b))
+
+    def chain_length(self):
+        return self._n
+
+    def num_x_values(self):
+        return self._k
+
+    def potential(self, i, a, b = None):
+        if b is None:
+            if i < 1 or i > self._n:
+                raise Exception("given n=" + str(self._n) + ", illegal value for i: " + str(i))
+            if a < 1 or a > self._k:
+                raise Exception("given k=" + str(self._k) + ", illegal value for a=" + str(a))
+            return self._potentials1[i][a]
+
+        if i < self._n + 1 or i > 2 * self._n - 1:
+            raise Exception("given n=" + str(self._n) + ", illegal value for i: " + str(i))
+        if a < 1 or a > self._k or b < 1 or b > self._k:
+            raise Exception("given k=" + str(self._k) + ", illegal value for a=" + str(a) + " or b=" + str(b))
+        return self._potentials2[i][a][b]
+
+
+class SumProduct:
+    def __init__(self, p):
+        self._potentials = p
+        self.k = self._potentials.num_x_values()
+        self.n = self._potentials.chain_length()
+        self.init_table()
+        # TODO: EDIT HERE
+        # add whatever data structures needed
+
+    def init_table(self):
+        table = np.ones((self.n, self.k, 3))
+        # build the left-neighbor direction, and then the right-neighbor
+        # direction starting from the opposite end
+        # use values precomputed for speed
+        # start at the 0th node (1-indexed 1)
+        table[0, :, 1] = self.get_current_message(0)
+        for i in range(1, self.n):
+            # get an array corresponding to the factor we just moved over
+            table[i,:,0] = self.get_factor_message(self.n + i - 1,
+                np.multiply(table[i-1,:,0], table[i-1,:,1]))
+            table[i,:,1]  = self.get_current_message(i)
+        table[self.n - 1, :, 1] = self.get_current_message(self.n - 1)
+        for i in reversed(range(0, self.n - 1)):
+            table[i,:,2] = self.get_factor_reverse(self.n + i,
+                np.multiply(table[i+1,:,2], table[i+1,:,1]))
+            table[i, :, 1] = self.get_current_message(i)
+        self.table = table
+        pass
+
+    # note that we keep all code to be 0-indexed internally, and marginal_probability
+    # swaps the x_i from 1 to zero indexing
+    def marginal_probability(self, x_i):
+        # TODO: EDIT HERE
+        # should return a python list of type float, with its length=k+1, and the first value 0
+        result = self.get_normalization(x_i-1)
+        result = np.divide(result, np.sum(result))
+        result = np.insert(result, 0, 0)
+        return result
+
+    # use this for normalization for sumprod
+    def get_normalization(self, x_i):
+        result = np.product(self.table[x_i,:,:], axis=1)
+        return result
+
+    # use this within maxsum
+    def get_total_potentials(self, x_i):
+        result = np.sum(np.log(self.table[x_i,:,:]), axis=1)
+        return result
+
+    # gets the message of a particular factor as we go from nodes 0 to n.
+    # this obtains the message from the preceding node, and multiplies it element wise
+    # with all the joints of a particular factor
+    def get_factor_reverse(self, factor_id, mu):
+        marginal = np.zeros((self.k, self.k))
+        for k1 in range(0, self.k):
+            for k2 in range(0, self.k):
+                marginal[k1, k2] = self._potentials.potential(factor_id + 1, k1 + 1, k2 + 1)*mu[k2]
+        marginal = marginal.sum(axis=1)
+        return marginal
+
+    # same idea as before
+    def get_factor_message(self, factor_id, mu):
+        marginal = np.zeros((self.k, self.k))
+        for k1 in range(0, self.k):
+            for k2 in range(0, self.k):
+                marginal[k1, k2] = self._potentials.potential(factor_id + 1, k2 + 1, k1 + 1)*mu[k2]
+        marginal = marginal.sum(axis=1)
+        return marginal
+
+    def get_current_message(self, x_i):
+        result = np.zeros((self.k))
+        for k in range(0, self.k):
+            result[k] = self._potentials.potential(x_i + 1, k + 1)
+        return result
+
+
+class MaxSum:
+    def __init__(self, p):
+        self._potentials = p
+        self.k = self._potentials.num_x_values()
+        self.n = self._potentials.chain_length()
+        self.init_table()
+        self.sumprod = SumProduct(p)
+        # TODO: EDIT HERE
+        # add whatever data structures needed
+
+    def init_table(self):
+        table = np.zeros((self.n, self.k, 3))
+        table[0, :, 1] = self.get_current_message(0)
+        for i in range(1, self.n):
+            # get an array corresponding to the factor we just moved over
+            # to go from i-1 to i, and use addition here since it is a max sum
+            table[i,:,0] = self.get_factor_message(self.n + i - 1,
+                np.add(table[i-1,:,0], table[i-1,:,1]))
+            table[i,:,1]  = self.get_current_message(i)
+        table[self.n - 1, :, 1] = self.get_current_message(self.n - 1)
+        for i in reversed(range(0, self.n - 1)):
+            table[i,:,2] = self.get_factor_reverse(self.n + i,
+                np.add(table[i+1,:,2], table[i+1,:,1]))
+            table[i, :, 1] = self.get_current_message(i)
+        self.table = table
+        pass
+
+    def get_assignments(self):
+        assignments = np.zeros((self.n))
+        table = self.table
+        for i in range(0, self.n):
+            # compute the argmax sum for all values, and add one for 0-indexing
+            # note that argmax automatically returns the minimum index
+            assignments[i] = np.argmax(table[i,:,:].sum(axis=1)) + 1
+        # put our zero pad on the front bc 0-indexing to 1-indexing
+        self._assignments = assignments
+        # make sure to return type as integers, since we are returning specific labels
+        return np.insert(self._assignments, 0, 0).astype(int)
+
+    def max_probability(self, x_i):
+        # consider the normalizing constant of the sumprod
+        norm = self.sumprod.get_normalization(x_i - 1)
+        # and normalize the max prob here by the log of the sum of the normalizing constant
+        return np.max(self.table[x_i-1,:,:].sum(axis=1)) - np.log(norm.sum())
+
+    def get_factor_reverse(self, factor_id, mu):
+        marginal = np.zeros((self.k, self.k))
+        for k1 in range(0, self.k):
+            for k2 in range(0, self.k):
+                # logs instead of direct probabilities, so addition in place of multiplication
+                # and logs anywhere we take a probability
+                marginal[k1, k2] = np.log(self._potentials.potential(factor_id + 1, k1 + 1, k2 + 1)) + mu[k2]
+        return marginal.max(axis=1)
+
+    def get_factor_message(self, factor_id, mu):
+        marginal = np.zeros((self.k, self.k))
+        for k1 in range(0, self.k):
+            for k2 in range(0, self.k):
+                marginal[k1, k2] = np.log(self._potentials.potential(factor_id + 1, k2 + 1, k1 + 1)) + mu[k2]
+        return marginal.max(axis=1)
+
+    def get_current_message(self, x_i):
+        result = np.zeros((self.k))
+        for k in range(0, self.k):
+            result[k] = self._potentials.potential(x_i + 1, k+1)
+        return np.log(result)
+
---
+++ homework_6/means.py
@@ -0,0 +1,137 @@
+# means.py
+# written by Eric Bridgeford
+#
+# a class for the lambda-means algorithm.
+#
+
+from cs475_types import Predictor
+from abc import ABCMeta, abstractmethod
+import numpy as np
+from scipy.spatial import distance
+from scipy.stats import mode
+
+
+class Lambda_Means(Predictor):
+    def __init__(self, lambd, iterations):
+        super(Lambda_Means, self).__init__()
+        self.lambd = lambd
+        self.iterations = iterations
+        self.K = 1
+        self.nfeatures = 0
+        pass
+
+    # load the example into an array
+    def load_example(self, instance):
+        example = np.zeros((self.nfeatures))
+        for idx in instance:
+            # since we have 1 indexed features but zero indexed arrays
+            example[idx - 1] = instance.get(idx)
+        return example[:self.nfeatures]
+
+    def get_feature(self, instance, feature):
+        return instance.get(feature + 1, default=0)
+
+    def calculate_max_feature(self):
+        for instance in self.instances:
+            self.nfeatures = max(instance.max_feature(), self.nfeatures)
+        self.ninstances = len(self.instances)
+        pass
+
+    # get the mean of a particular feature based on a responsibility
+    # vector.
+    def get_feature_mean(self, resp, feat_id):
+        this_feature = np.ones((self.ninstances))
+        instances = self.instances
+        for idx in range(0, self.ninstances):
+            this_feature[idx] = self.get_feature(instances[idx], feat_id)
+        return np.mean(this_feature)
+
+    def initialize(self):
+        # will be the mean of all of these
+        self.resp = np.ones((self.ninstances, self.K))         
+        self.update_means()
+        # mean of the dataset will be the first cluster
+        mean_vec = self.means[:,0]
+        if (self.lambd <= 0):
+            instances = self.instances
+            distance = np.zeros((self.ninstances))
+            for idx in range(0, self.ninstances):
+                x = self.load_example(instances[idx])
+                distance[idx] = self.squared_distance(x, mean_vec)
+            self.lambd = np.mean(distance)
+        pass
+
+    def squared_distance(self, x, y):
+        return distance.euclidean(x, y)**2
+
+    # a function to compute the distance between a single datum and
+    # all of the clusters we have
+    def compute_distance(self, x, means):
+        distances = np.zeros((self.K))
+        for k in range(0, self.K):
+            distances[k] = self.squared_distance(x, means[:,k])
+        return distances
+
+    def get_responsibilities(self, lambd):
+        r = np.zeros((self.ninstances, self.K))
+        # load the means locally for speed
+        means = self.means
+        for idx in range(0, self.ninstances):
+            x = self.load_example(self.instances[idx])
+            distance = self.compute_distance(x, means)
+            dist_idx = np.argmin(distance)
+            if distance[dist_idx] <= lambd:
+                # set the responsibility to 1, will be 0 otherwise
+                r[idx, dist_idx] = 1
+            # make a new cluster if none of the current clusters
+            # satisfy the lambda requirement
+            else:
+                means = np.column_stack((means, x))
+                # add a responsibility vector that defaults to zeros
+                # and set the value of this instance to 1
+                new_rvec = np.zeros((self.ninstances, 1))
+                new_rvec[idx] = 1
+                r = np.column_stack((r, new_rvec))
+                self.K = self.K + 1
+        self.means = means
+        self.resp = r
+        pass
+
+    def update_means(self):
+        # use these locally for speed bump
+        K = self.K
+        r = self.resp
+        means = np.zeros((self.nfeatures, K))
+        instances = self.instances
+        for k in range(0, K):
+            r_vec = r[:,k]
+            # get the instance ids we want so we don't have to
+            # check all of them
+            r_ids = np.where(r_vec != 0)[0]
+            for idx in r_ids:
+                means[:,k] += self.load_example(instances[idx])
+            if (r_ids.shape[0] != 0):
+                means[:,k] = means[:,k]/float(r_ids.shape[0])
+        # update with our maximized means
+        self.means = means
+        pass
+
+    def train(self, instances):
+        self.instances = instances
+        self.calculate_max_feature()
+        # initialize the parameters of the model (resps and means)
+        self.initialize()
+        for i in range(0, self.iterations):
+            # get the respnsibilities, E step
+            self.get_responsibilities(self.lambd)
+            # maximize the means, M step
+            self.update_means()
+        pass
+
+    # make predictions baed on the shortest distance cluster to
+    # a particular example
+    def predict(self, instance):
+        x = self.load_example(instance)
+        distance = self.compute_distance(x, self.means)
+        # return minimum cluster id that has the minimum distance
+        return np.min(np.argmin(distance))
